03e2f8ba18f1df626751ec61465dd5acad7b5dbc
binhfoto <naibinh@gmail.com>
2016-10-17 19:57:08 +0700

    test population in mongoose

--- server/api/category/route.js
+++ server/api/category/route.js
@@ -1,10 +1,17 @@
 var router = require('express').Router();
 var logger = require('../../util/logger.js');
+var controller = require('./controller.js');
+
+route.param('id', controller.params);
 
 router.route('/')
-    .get(function(req, res) {
-        logger.log('Hey from category!!');
-        res.send({ok: true});
-});
+    .get(controller.get)
+    .post(controller.post);
+
+router.route('/:id')
+    .get(controller.getOne)
+    .put(controller.put)
+    .delete(controller.delete) ;
+
 
 module.exports = router;
